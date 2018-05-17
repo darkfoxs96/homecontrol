989a94735f6732b47077786c9c6e0fa871b1a73a
peterksts <peterk@silvertreesystems.com>
2018-05-17 19:34:29 +0300

    Add package controlsystemhome

--- goserver/services/controlled/requestcontrolled.go
+++ goserver/services/controlled/requestcontrolled.go
@@ -7,12 +7,13 @@ import (
 	"net/http"
 
 	"homecontrol/goserver/models"
+	"homecontrol/goserver/services/controlsystemhome"	
 )
 
 // RequestToControlled action request
 func RequestToControlled(controlled *models.Сontrolled, command *models.CommandRecord) (responseMessage string, err error) {
-	if controlled.HomeControlID != 0 {
-		// TODO: Request to homecontrol
+	if controlled.HomeControlID != "" {
+		responseMessage, err = controlsystemhome.ControlSystemHomeInterfaces[controlled.HomeControlID].RequestToHomeControlSystem(controlled, command)
 		return
 	}
 	buffer := bytes.NewBuffer(nil)
@@ -47,7 +48,7 @@ func RequestToControlled(controlled *models.Сontrolled, command *models.Command
 }
 
 // RegistrationAndUpdateControlled create record 'controlled' or update
-func RegistrationAndUpdateControlled(controlledID int, host, port, name string, commonBuffer, homeControlID int) (controlledAddID int, err error) {
+func RegistrationAndUpdateControlled(controlledID int, host, port, name string, commonBuffer int, homeControlID string) (controlledAddID int, err error) {
 	controlled := models.Сontrolled{
 		Host:          host,
 		Port:          port,
@@ -76,4 +77,32 @@ func GetControlled(ID int) (controlled *models.Сontrolled) {
 // DeleteControlled delete controlled bu ID
 func DeleteControlled(ID int) error {
 	return models.DeleteСontrolled(ID)
+}
+
+// GetInfoControlledsString return in arbitrary format
+func GetInfoControlledsString() (str string, err error) {
+	strStart := "Information on controlled:\n"
+	controlleds := models.GetСontrolleds()
+	command := &models.CommandRecord{
+		Command:       12,
+	}
+
+	for _, controlled := range controlleds {
+		if controlled.HomeControlID != "" {
+			continue
+		}
+		_, err := RequestToControlled(controlled, command)
+		if err == nil {
+			str += controlled.Name + ": Work\n"
+		} else {
+			str += controlled.Name + ": Not work\n"
+		}
+	}
+
+	if str == "" {
+		str += "No controlled\n"
+	}
+	
+	str = strStart + str
+	return
 }
