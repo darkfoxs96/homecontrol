a06ef6e1c465974159da3b65057d2c7fd6c7af40
peterksts <peterk@silvertreesystems.com>
2018-05-21 11:43:33 +0300

    Added package goserve/services/commands

--- goserver/services/commandrecord/record.go
+++ goserver/services/commandrecord/record.go
@@ -89,3 +89,37 @@ func AddCommandRecord(record *models.CommandRecord, ID string) error {
 func DeleteCommandRecord(ID string) error {
 	return models.DeleteCommandRecord(ID)
 }
+
+// GetListCommands return controlled commands list
+func GetListCommands() (commandsList *models.ListCommands) {
+	commandsList.Name = "controlled"
+	commandsList.StartRangeIDCommands = 0
+	commandsList.EndRangeIDCommands = 999
+	commandsList.Commands =
+		[]*models.Command{&models.Command{
+			ID: 0, InfoCommand: "Put buffer",
+		}, &models.Command{
+			ID: 1, InfoCommand: "Open page from the buffer",
+		}, &models.Command{
+			ID: 2, InfoCommand: "Stop",
+		}, &models.Command{
+			ID: 3, InfoCommand: "Sound off",
+		}, &models.Command{
+			ID: 4, InfoCommand: "Sound on",
+		}, &models.Command{
+			ID: 5, InfoCommand: "off",
+		}, &models.Command{
+			ID: 6, InfoCommand: "Open youtube",
+		}, &models.Command{
+			ID: 7, InfoCommand: "Open vk",
+		}, &models.Command{
+			ID: 8, InfoCommand: "Open ok",
+		}, &models.Command{
+			ID: 9, InfoCommand: "Open fecebook",
+		}, &models.Command{
+			ID: 10, InfoCommand: "Open",
+		}, &models.Command{
+			ID: 11, InfoCommand: "Used code in terminal",
+		}}
+	return
+}
